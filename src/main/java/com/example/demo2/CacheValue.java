bb8b96ea236a9aa9f9fb96943b2722dbe1c3df92
ty1151933867 <1151933867@qq.com>
2026-09-21 21:44:14 +0800

    senond commit

--- src/main/java/com/example/demo2/CacheValue.java
+++ src/main/java/com/example/demo2/CacheValue.java
@@ -17,6 +17,6 @@ public class CacheValue {
         sheet.getRange("A5").setValue(1000);
         XlsxSaveOptions xlsxSaveOptions = new XlsxSaveOptions();
         xlsxSaveOptions.setIsCompactMode(false);
-        workbook.save("cache.xlsx", xlsxSaveOptions);
+        workbook.save("cache_1.2.0.xlsx", xlsxSaveOptions);
     }
 }
